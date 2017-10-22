0b98e54d343dc764cc72e6df96ab3f07015190ac
Steven Iglesias <steveniglesias@hotmail.com>
2017-10-22 00:52:21 -0500

    Register and Login added
    -Added Register and Login Views
    -Added functions for registration and login
    -Added redirections to Login and Index when registrated and logged in
    respectively.
    ------------For Register
    -Added data annotations for validation to Account entity (Regex,
    Required, Datatype and string length)
    -When registrating, error messages are displayed when a username or mail
    is already taken
    ------------For login
    -On successfully registration, a message is sent to the login view
    -When logging in, if a username is not on the collections, or password
    doesn't match, an error message is displayed above.
    -When successfully logged, username of current user is saved in Session
    variable.

--- DuLink/Controllers/AccountController.cs
+++ DuLink/Controllers/AccountController.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DuLink.Models;
+using MongoDB.Driver;
+using DuLink.Entities;
+using System.Configuration;
 
 namespace DuLink.Controllers
 {
     public class AccountController : Controller
     {
+        private MongoClient mongoClient;
+        private IMongoCollection<Account> accountCollection;
         // GET: Account
         public ActionResult Index()
         {
@@ -16,5 +21,118 @@ namespace DuLink.Controllers
             ViewBag.LISTA = account.FindAll();
             return View();
         }
+
+        public void iniAccountList()
+        {
+            if (accountCollection == null)
+            {
+                mongoClient = new MongoClient(ConfigurationManager.AppSettings["mongoDBHost"]);
+                var db = mongoClient.GetDatabase(ConfigurationManager.AppSettings["mongoDBName"]);
+                accountCollection = db.GetCollection<Account>("Employee");
+            }
+        }
+
+
+        /*****************************************/
+        /*****************LOGIN****************/
+        /*****************************************/
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            iniAccountList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Account currentUser)
+        {
+            iniAccountList();
+            if (loginMatches(currentUser))
+            {
+                Session["CurrentErrorMessage"] = null;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        private bool loginMatches(Account loggedUser)
+        {
+            List<Account> allUsers = accountCollection.AsQueryable<Account>().ToList();
+            foreach (Account user in allUsers)
+            {
+                if (user.UserName.Equals(loggedUser.UserName))
+                {
+                    if (user.Password.Equals(loggedUser.Password))
+                    {
+                        Session["Username"] = loggedUser.UserName;
+                        return true;
+                    }
+                    else
+                    {
+                        Session["CurrentErrorMessage"] = "Password for this user is incorrect!";
+                        return false;
+                    }
+                }
+            }
+            Session["CurrentErrorMessage"] = "No user was found with username: " + loggedUser.UserName;
+            return false;
+        }
+
+        /*****************************************/
+        /*****************REGISTRO****************/
+        /*****************************************/
+
+        public ActionResult Registro()
+        {
+            iniAccountList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registro(Account nuevoUser)
+        {
+            iniAccountList();
+            if (ModelState.IsValid)
+            {
+                if (isUserAllowed(nuevoUser))
+                {
+                    accountCollection.InsertOne(nuevoUser);
+                    Session["CurrentErrorMessage"] = "Registration Successful, now log in!";
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                Session["CurrentErrorMessage"] = null;
+            }
+            return View(nuevoUser);
+        }
+
+        
+        private bool isUserAllowed(Account usuario) {
+            List<Account> allUsers = accountCollection.AsQueryable<Account>().ToList();
+            foreach (Account user in allUsers)
+            {
+                if (user.UserName.Equals(usuario.UserName))
+                {
+                    Session["CurrentErrorMessage"] = "This username is already taken!";
+                    return false;
+                }
+                if (user.Mail.Equals(usuario.Mail))
+                {
+                    Session["CurrentErrorMessage"] = "This mail is already taken!";
+                    return false;
+                }
+            }
+            return true;
+        }
+
     }
 }
