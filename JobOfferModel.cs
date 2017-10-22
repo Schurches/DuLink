26c7840b342f385f8f60b62a0662483cd3dbb6f9
johan <jdavid.ilias@gmail.com>
2017-10-22 09:23:43 -0500

    Funciones de búsqueda mejoradas
    -busqueda en los modelos de Account y JobOffer mejoradas

---
+++ JobOfferModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Configuration;
+using DuLink.Entities;
+
+namespace DuLink.Models
+{
+    public class JobOfferModel
+    {
+        private MongoClient mongoClient;
+        private IMongoCollection<JobOffer> jobOfferCollection;
+
+        public JobOfferModel()
+        {
+            mongoClient = new MongoClient(ConfigurationManager.AppSettings["mongoDBHost"]);
+            var db = mongoClient.GetDatabase(ConfigurationManager.AppSettings["mongoDBName"]);
+            jobOfferCollection = db.GetCollection<JobOffer>("EmploymentOffer");
+        }
+
+        public List<JobOffer> FindAll()
+        {
+            return jobOfferCollection.AsQueryable<JobOffer>().ToList();
+        }
+
+        public JobOffer FindJobOffer(string id)
+        {
+            var accountId = new ObjectId(id);
+            return jobOfferCollection.AsQueryable<JobOffer>().SingleOrDefault(a => a.Id == accountId);
+        }
+
+        public void CreateJobOffer(JobOffer jobOffer)
+        {
+            jobOfferCollection.InsertOne(jobOffer);
+        }
+
+        public List<JobOffer> FindAllByDescription(String keyWords)
+        {
+            List<JobOffer> listaResult = new List<JobOffer>();
+            if (keyWords.Trim() != "")
+            {
+                List<JobOffer> lista = jobOfferCollection.AsQueryable<JobOffer>().ToList();
+                String[] keys = keyWords.Replace(' ', ';').Split(';');
+                foreach (JobOffer i in lista)
+                {
+                    foreach (String j in keys)
+                    {
+                        if (j != "")
+                        {
+                            if (i.Description.Contains(keyWords))
+                            {
+                                listaResult.Add(i);
+                            }
+                        }
+                    }
+
+                }
+            }
+ 
+            return listaResult;
+        }
+        public List<JobOffer> FindAllByCompanyName(String keyWords)
+        {
+            List<JobOffer> listaResult = new List<JobOffer>();
+            if (keyWords.Trim() != "")
+            {
+                List<JobOffer> lista = jobOfferCollection.AsQueryable<JobOffer>().ToList();
+                String[] keys = keyWords.Replace(' ', ';').Split(';');
+                foreach (JobOffer i in lista)
+                {
+                    foreach (String j in keys)
+                    {
+                        if (j != "")
+                        {
+                            if (i.CompanyName.Contains(keyWords))
+                            {
+                                listaResult.Add(i);
+                            }
+                        }
+                    }
+
+                }
+            }
+            return listaResult;
+        }
+
+    }
+}
